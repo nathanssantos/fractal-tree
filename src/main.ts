15ea6661fe665a7566599e9c2ec329b5de6d5dd8
Nathan Silva Santos <nathan@w16.com.br>
2022-05-15 01:01:51 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,81 @@
+import './style.css';
+
+const ground = document.querySelector<HTMLDivElement>('#ground')!;
+
+const random = (min: number, max: number): number =>
+  Math.random() * (max - min) + min;
+
+const createBranch = (
+  rotation: number,
+  size: number,
+  width: number,
+  bottom: string = '90%'
+): HTMLDivElement => {
+  const newBranch = document.createElement('div');
+
+  newBranch.style.backgroundColor = '#000';
+  newBranch.style.width = `${width}px`;
+  newBranch.style.height = `${size}px`;
+
+  newBranch.style.position = 'absolute';
+  newBranch.style.left = '50%';
+  newBranch.style.bottom = bottom;
+
+  newBranch.style.transformOrigin = 'bottom';
+
+  newBranch.style.transform = `rotate(${
+    rotation * random(0.8, 1.5)
+  }deg) translate(-50%)`;
+
+  return newBranch;
+};
+
+const fertilizeTree = (
+  size: number,
+  branches: HTMLDivElement[],
+  branchSize: number,
+  branchWidth: number,
+  rotation: number
+) => {
+  if (branches?.length > size) return;
+
+  const newBranches = [];
+
+  for (const branch of branches) {
+    const leftBranch = createBranch(
+      -rotation,
+      branchSize * random(0.6, 0.85),
+      branchWidth * random(0.6, 0.85)
+    );
+    const middleBranch = createBranch(
+      0,
+      branchSize * random(0.95, 1.05),
+      branchWidth * random(0.95, 1.05)
+    );
+    const rightBranch = createBranch(
+      rotation,
+      branchSize * random(0.6, 0.85),
+      branchWidth * random(0.6, 0.85)
+    );
+
+    branch.append(leftBranch, middleBranch, rightBranch);
+
+    newBranches.push(leftBranch, middleBranch, rightBranch);
+  }
+
+  fertilizeTree(
+    size,
+    newBranches,
+    branchSize * random(0.6, 0.85),
+    branchWidth * random(0.45, 0.55),
+    rotation
+  );
+};
+
+const mainBranchSize = random(100, 160);
+
+const tree = createBranch(0, mainBranchSize, 30, '0');
+
+ground.append(tree);
+
+fertilizeTree(640, [tree], mainBranchSize, 20, 27);
